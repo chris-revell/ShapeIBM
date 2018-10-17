27bc1ff4ea2c0cea5a5edc9cc60d6e75bb34d70c
chris-revell <cr395@cam.ac.uk>
2018-10-17 09:47:38 -0400

    Updates

--- plotter.py
+++ plotter.py
@@ -9,3 +9,4 @@ nsteps = int(np.shape(data)[0]/npoints)
 for step in range(nsteps):
     fig, ax = plt.subplots(figsize=(4,4))
     ax.scatter(data[step*npoints:(step+1)*npoints,0],data[step*npoints:(step+1)*npoints,1])
+    
