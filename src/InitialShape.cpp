b14068e5ef2c55e812c747d6a1b92b1ef3df9bae
chris-revell <ckrevell@gmail.com>
2019-09-02 09:05:03 +0100

    Initialising 2nd shape option

--- src/InitialShape.cpp
+++ src/InitialShape.cpp
@@ -30,4 +30,42 @@ bool InitialShape(const int& i,const int& j,const float& len, const float& h, co
       return false;
     }
   }
+
+
+
+
+
+  
+
+  for (int i = 0; i < Numg; i++) {
+    float x = (i-Numg/2)*Tissue.hg;
+    for (int j = 0; j < Numg; j++) {
+      float y = (j-Numg/2)*Tissue.hg;
+      //float y_1 = b*(alpha-1)*fabs(x)/(2*h)+b/2;
+      //float y_0 = b*(1-alpha)*fabs(x)/(2*h)-b/2;
+      //if (x>-h && x<h && y<y_1 && y>y_0){
+      //  PositionGrid(i,j)=1;
+      //}
+      //else{
+      //  PositionGrid(i,j)=0;
+      //}
+      float y_1 = b*(alpha-1)*x/(2*h)+b*(1+alpha)/4;
+      float y_0 = b*(1-alpha)*x/(2*h)-b*(1+alpha)/4;
+      if (x>(-h/2) && x<(h/2) && y<y_1 && y>y_0){
+        PositionGrid(i,j)=1;
+      }
+      else{
+        PositionGrid(i,j)=0;
+      }
+    }
+  }
+
+
+
+
+
+
+
+
+
 }
