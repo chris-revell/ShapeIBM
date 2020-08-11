33dc1dcf8a6fada017d6da1ed6ec66bc0c14981b
chris-revell <christopher.revell@crick.ac.uk>
2020-08-11 13:33:52 +0100

    Update runscript1.sh

--- runscript1.sh
+++ runscript1.sh
@@ -1,9 +1,7 @@
 
-for i in $(seq 0.1 0.3 1.0); do
-  for j in $(seq 0.6 0.3 1.5); do
-    echo $i 
-    echo $j
-    ./shapeIBM 256 1.0 1.0 0.75 0.2 $i 0.5 50.0 10.0 20 $j 1 0.1 1500 100 1 0 0 >> /dev/null
+for i in $(seq 0.0 0.1 1.0); do
+  for j in $(seq 0.0 0.1 1.5); do
+    ./shapeIBM 256 1.0 1.0 0.75 0.2 $i 0.5 50.0 10.0 20 $j 1 0.1 1500 100 0 0 1>> /dev/null 2>&1 & sleep 10;
   done
+  wait
 done
-
